e34968f0c912abc4a3e2c4dd8f84c81848cf05ad
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2025-02-22 00:40:26 +0400

    .

--- recursive/src/main.rs
+++ recursive/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Recursive!");
+    let n = 5;
+    println!("Head Recursion Factorial of {} is {}", n, fact_head(n));
+
+    println!("Tail Recursion Factorial of {} is {}", n, fact_tail(n));
+}
+
+
+
+//tail recursive
+fn fact_tail(n: u64) -> u64 {
+    fn fact_iter(n: u64, acc: u64) -> u64 {
+        if n == 0 {
+            acc
+        } else {
+            fact_iter(n - 1, n * acc)
+        }
+    }
+    fact_iter(n, 1)
+}
+
+//head recursive
+fn fact_head(n: u64) -> u64 {
+    if n == 0 {
+        1
+    } else {
+        n * fact_head(n - 1)
+    }
 }
