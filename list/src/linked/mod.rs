85fb3a70acf19f1cb3a4670143ce1da85202238b
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2025-02-22 15:59:37 +0400

    linked list

--- list/src/linked/mod.rs
+++ list/src/linked/mod.rs
@@ -7,7 +7,7 @@ pub struct List<T> {
 
 
 
-impl <T> List<T> {
+impl <T: std::cmp::PartialEq + Copy> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
@@ -20,11 +20,51 @@ impl <T> List<T> {
         self.head = Some(new_node);
     }
 
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.data)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.data)
+    }
+
     pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.data
         })
+    }
+
+    pub fn remove(&mut self, elem: T) -> Option<T> {
+        let mut current = self.head.as_mut();
+        match current {
+            None => None,
+            Some(ref mut node) => {
+                if node.data == elem {
+                    self.pop()
+                } else {
+                    while let Some(ref mut n) = current {
+                        if n.data == elem {
+                            let next = n.next.take();
+                            let data = n.data;
+                            n.next = next;
+                            return Some(data);
+                        }
+                        current = n.next.as_mut();
+                    }
+                    None
+                }
+            }
+        }
+    }
+}
+
+impl <T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut current = self.head.take();
+        while let Some(mut node) = current {
+            current = node.next.take();
+        }
     }
 }
 
@@ -39,6 +79,7 @@ struct Node<T> {
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Deref;
     use super::*;
 
 
@@ -50,6 +91,37 @@ mod tests {
             list.push(i);
         }
         assert_eq!(list.pop(), Some(&5));
+    }
+
+    #[test]
+    fn test_linked_list_remove_contains_element(){
+        let arr = vec![1,2,3,4,5];
+        let mut list = List::new();
+        for i in arr.iter() {
+            list.push(i);
+        }
+        assert_eq!(list.remove(&3), Some(&3));
+    }
+
+    #[test]
+    fn test_linked_list_remove_does_not_contain_element(){
+        let arr = vec![1,2,3,4,5];
+        let mut list = List::new();
+        for i in arr.iter() {
+            list.push(i);
+        }
+        assert_eq!(list.remove(&6), None);
+    }
+
+    #[test]
+    fn test_linked_list_peek(){
+        let arr = vec![1,2,3,4,5];
+        let mut list = List::new();
+        for i in arr.iter() {
+            list.push(i);
+        }
+        let elem = list.peek().unwrap().deref();
+        assert_eq!(elem, &5);
     }
 
 }
