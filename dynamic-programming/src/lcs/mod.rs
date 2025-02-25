84ed924bf8e708fec5fc8908b7fd048e8a18e205
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2025-02-25 23:25:32 +0400

    Longest Common Subsequence

--- dynamic-programming/src/lcs/mod.rs
+++ dynamic-programming/src/lcs/mod.rs
@@ -9,6 +9,24 @@ pub fn lcs_resolve(a: &str, b: &str) -> Option<String>{
 
     let (len_a, len_b) = (a.len(),b.len());
 
+    let mut solution = vec![vec![0,len_b +1];len_a + 1];
+
+    for (i, mi) in a.iter().enumerate() {
+        for (j, mj) in b.iter().enumerate() {
+            solution[i+1][j+1] = if mi == mj {
+                solution[i][j] + 1
+            }else {
+                solution[i][j+ 1].max(solution[i+1][j])
+            }
+        }
+    }
+
+    let mut result:Vec<char> = Vec::new();
+
+    let (mut i, mut j) = (len_a,len_b);
+    while i > 0 && j > 0 {
+
+    }
     None
 }
 
